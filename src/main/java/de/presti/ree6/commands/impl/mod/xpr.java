b4303c77377c45c12cb24ca67c1958f258140f4b
Gru <ruslru376@gmail.com>
2024-07-23 23:44:45 +1000

    Build fixes and /xpr

---
+++ src/main/java/de/presti/ree6/commands/impl/mod/xpr.java
@@ -0,0 +1,61 @@
+package de.presti.ree6.commands.impl.mod;
+
+import de.presti.ree6.commands.Category;
+import de.presti.ree6.commands.CommandEvent;
+import de.presti.ree6.commands.interfaces.Command;
+import de.presti.ree6.commands.interfaces.ICommand;
+import de.presti.ree6.language.LanguageService;
+import de.presti.ree6.main.Main;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import net.dv8tion.jda.internal.interactions.CommandDataImpl;
+
+import java.time.Duration;
+
+/**
+ * A command to mute a user.
+ */
+@Command(name = "xpr", description = "Позволяет писать от имени бота", category = Category.MOD)
+public class xpr implements ICommand {
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public void onPerform(CommandEvent commandEvent) {
+        if (!commandEvent.isSlashCommand()) {
+            commandEvent.reply(commandEvent.getResource("command.perform.onlySlashSupported"));
+            return;
+        }
+        if (commandEvent.isSlashCommand()) {
+
+            OptionMapping reasonOption = commandEvent.getOption("text");
+
+            commandEvent.getChannel().sendMessage(reasonOption.getAsString());
+
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public CommandData getCommandData() {
+        return new CommandDataImpl("xpr", "Отправить сообщение от имени бота")
+                .addOptions(new OptionData(OptionType.STRING, "text", "Тест, который нужно отправить.").setRequired(true))
+                .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.MODERATE_MEMBERS));
+    }
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public String[] getAlias() {
+        return new String[0];
+    }
+}
